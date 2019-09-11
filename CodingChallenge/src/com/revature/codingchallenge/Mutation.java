54e63ea11c5395ae76adcdba23d45bb95c29f29f
Khanh N.Q Dang <knd7@njit.edu>
2019-09-10 22:20:35 -0400

    Update Java Class with code documentation

--- CodingChallenge/src/com/revature/codingchallenge/Mutation.java
+++ CodingChallenge/src/com/revature/codingchallenge/Mutation.java
@@ -34,7 +34,7 @@ public class Mutation
 	
 	
 	//read gene bank from file
-	public static void readFile()
+	public static void readFile() //throw exception again here?
 	{
 		StringBuilder sb = new StringBuilder();
 		File file =  new File("src/com/revature/codingchallenge/genebank.txt");
@@ -53,55 +53,60 @@ public class Mutation
 
 		} catch (IOException e) {
 			System.out.println("No gene bank txt file found, program will terminate !!!");
-
+			
 		}
 		
 		//br.close?
 		start = bank.get(0);
 		end = bank.get(1);
-		String temp = bank.get(2);
-		banks = temp.split(",");
+		String tempString = bank.get(2);
+		banks = tempString.split(",");
 
 	}
 
-	//Method to find minMutation with LinkedList and Hashset using BFS
+	//Method to find minMutation with LinkedList and Hashset using BFS to compare two nodes
 	public static int minMutation(String start, String end, String[] bank) 
 	{
+		//Create the queue for the start string and create hashset for the banks 
+		//Hash set doesn't allow duplicate entries
 		Queue<String> queue = new LinkedList<>();
 		char[] chars = {'A', 'C', 'G', 'T'};
 		Set<String> bankSet = new HashSet<>();
 
-		int temp = 0;
+		int temp = 0; // counter
+		// the queue offer is a method that doen't throws an exception 
+		//when the capacity of the container is full since it returns false
+		
 		queue.offer(start);
 
 		for (String b : bank) 
 		{
 			bankSet.add(b);
 		}
-		while(!queue.isEmpty()) 
+		while(!queue.isEmpty()) //starting BFS algorithm to search for the differences between two nodes
 		{
-			int size = queue.size();
+			int size = queue.size();// returns the n of elements in the queue
 			while(size-- > 0) 
 			{
-				String currString = queue.poll();
+				String currString = queue.poll(); // removes and returns at the head of queue. Returns null if empty
 				if (currString.equals(end)) return temp;
 				for (int i = 0; i < currString.length(); i++) 
 				{
-					for (char ch: chars)
+					for (char ch: chars)// Checking between two nodes
 					{
-						char[] currChars = currString.toCharArray();
+						char[] currChars = currString.toCharArray();// returns an Array of chars after converting a string to chars
 						currChars[i] = ch;
 						String modString = new String(currChars);
-						if (bankSet.contains(modString)) 
+						if (bankSet.contains(modString)) // check to see if new mutate is in the bank, and not the original duplicate
 						{
-							queue.add(modString);
-							bankSet.remove(modString);
+							queue.add(modString); //add the next gen into the queue
+							bankSet.remove(modString); // prevent backtracking by removing the instance of mutation from the set
 						}
 					}
 				}
 			}
-			temp++;
+			temp++; //count for next gen
 		}
-		return -1;
+		return -1; // when fail
 	}
 }
