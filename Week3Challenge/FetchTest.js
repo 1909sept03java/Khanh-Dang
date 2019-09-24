a698ebd4a4e66388e988e736fe82eb109e4760b0
Khanh N.Q Dang <knd7@njit.edu>
2019-09-23 22:56:15 -0400

    Added Extra css style sheet and show average age

--- Week3Challenge/FetchTest.js
+++ Week3Challenge/FetchTest.js
@@ -1,12 +1,14 @@
 //Testing Fetch file works with no event handler at the mean time. 
-const url = "https://randomuser.me/api/?results=25";
-const ul = document.getElementById("people");
 
-function createUser(e) { // create user func
+const ul = document.getElementById("people"); // keep constant for the file
+var averageValue = 0;
+var sum = 0;
+
+function createUser(e) { // create user func ->parent func allow use to create an element 
    return document.createElement(e);
 }
-function append(parent, e) {
-   return parent.appendChild(e); // Append the second element to the first element in parent
+function appendUser(parent, e) {
+   return parent.appendChild(e); // Append the second element(child) to the first element in parent funct -> method appends a node as the last child of a node.
 }
 
 function ageAverage(arr){
@@ -21,28 +23,33 @@ function ageAverage(arr){
    return (sum / numbCount);
 }
 
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
+fetch("https://randomuser.me/api/?results=25")
+.then((resp) => resp.json()) // make first request to transfrom data into json
+.then(function(data) {// get users data
  let users = data.results;
- return users.map(function(user) {
-   let li = createUser('li'),
-       img = createUser('img'),
-       p = createUser('p');
-       a = createUser('a');
-   img.src = user.picture.thumbnail;
-   p.innerHTML = `${user.name.first} ${user.name.last}`;
-   a.innerHTML = `${user.dob.age}`;
-   append(li, img);
-   append(li, p);
-   append(ul, li);
-   append(li, a);
+ return users.map(function(user) { // use .map to return data with keys and values into the results array. 
+   let li = createUser('li'), // lists of users
+       img = createUser('img'), // user image
+       p = createUser('p'); // users name
+       a = createUser('a');// users age
+       c = createUser('c'); // users location
+      
+   img.src = user.picture.large; // using API documentation
+   p.innerHTML = `Name: ${user.name.first} ${user.name.last}`; //modifying html file, populating the results
+   a.innerHTML = `Age: ${user.dob.age}`;
+   c.innerHTML = `Location: ${user.location.city}, ${user.location.state}`;
+   averageValue = averageValue + user.dob.age;
+   appendUser(li, img); // append new child elements to the parent.
+   appendUser(li, p);
+   appendUser(ul, li);
+   appendUser(li, a);
+   appendUser(li, c);
 
-   /*sum += user.dob.age;
-       avg = sum/25;
-       avgAge = document.getElementById("averageAge");
-       avgAge.innerHTML = 'Average age: ${avg}';
-       console.log(avg);*/
+   // calculate avg age at the bottom when the 25 ages are shown already.
+   averageAge = document.getElementById("averageAge");
+   sum = averageValue/25;
+   averageAge.innerHTML = `Show average age of these 25 users: ${sum}`;
+
  })
 })
 .catch(function(error) {
